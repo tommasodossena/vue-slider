a5fa4a16b135beedcd4ef84f46648b6347fe21f5
Tommaso <dossena.tommaso.dt@gmail.com>
2021-03-25 16:33:57 +0100

    Added data objects

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 var vm = new Vue(
 	{
 		el: '#root',
+        data: {
+            img: [
+                {
+                    url: "https://via.placeholder.com/1080x720",
+                    descrizione: "placeholder",
+                }
+            ],
+            imgIndex: 0
+        },
+        methods: {
+
+        }
 	}
 );
