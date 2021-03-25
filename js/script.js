311fbe394e4fa5ac7d3ebef11475fad7d3496c17
Tommaso <dossena.tommaso.dt@gmail.com>
2021-03-25 17:08:52 +0100

    Carousel chevron event

--- js/script.js
+++ js/script.js
@@ -4,14 +4,39 @@ var vm = new Vue(
         data: {
             img: [
                 {
-                    url: "https://via.placeholder.com/1080x720",
+                    url: "https://via.placeholder.com/800x400/ef476f/FFFFFF?text=Slide+1",
+                    descrizione: "placeholder",
+                },
+                {
+                    url: "https://via.placeholder.com/800x400/ffd166/FFFFFF?text=Slide+2",
+                    descrizione: "placeholder",
+                },
+                {
+                    url: "https://via.placeholder.com/800x400/06d6a0/FFFFFF?text=Slide+3",
+                    descrizione: "placeholder",
+                },
+                {
+                    url: "https://via.placeholder.com/800x400/118ab2/FFFFFF?text=Slide+4",
                     descrizione: "placeholder",
                 }
             ],
             imgIndex: 0
         },
         methods: {
+            nextImage: function() {
+                this.imgIndex++;
 
+                if ( this.imgIndex == this.img.length ) {
+                    this.imgIndex = 0;
+                }
+            },
+            prevImage: function() {
+                this.imgIndex--;
+
+                if ( this.imgIndex < 0 ) {
+                    this.imgIndex = this.img.length - 1;
+                }
+            }
         }
 	}
 );
